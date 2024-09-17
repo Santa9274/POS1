9ad5f03b53e16079662b5e2844f7625a88094a8f
Santa9274 <sandiego.friedwagner07@gmail.com>
2024-09-17 12:08:02 +0200

    algorithmFinished

--- EratosthenesPrimeSieve/src/net/htlgkr/pos3/FriedwagnerS22040/EratosthenesPrimeSieve.java
+++ EratosthenesPrimeSieve/src/net/htlgkr/pos3/FriedwagnerS22040/EratosthenesPrimeSieve.java
@@ -1,8 +1,11 @@
 package net.htlgkr.pos3.FriedwagnerS22040;
 
+import java.sql.Array;
+
 public class EratosthenesPrimeSieve
 {
     private int limit;
+    boolean[] primes = new boolean[limit];
 
     public EratosthenesPrimeSieve(int limit) {
         this.limit = limit;
@@ -10,9 +13,21 @@ public class EratosthenesPrimeSieve
 
     public boolean isPrime(int p)
     {
+        //alles auf false
+        for (int i = 2; i <= p; i++) {
+            primes[i] = true;
+        }
 
-
-        return true;
+        for (int i = 2; i <= Math.sqrt(limit); i++)
+        {
+            if (primes[i])
+            {
+                for (int j = i*i; j<= limit; j+=i) {
+                    primes[j] = false;
+                }
+            }
+        }
+        
     }
 
     public void printPrime()
