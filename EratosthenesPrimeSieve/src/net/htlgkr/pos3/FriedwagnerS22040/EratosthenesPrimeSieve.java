4827802d33163d12a3224fed5ca06ef1a78e06fc
Santa9274 <sandiego.friedwagner07@gmail.com>
2024-09-17 11:38:23 +0200

    methodsCreated

--- EratosthenesPrimeSieve/src/net/htlgkr/pos3/FriedwagnerS22040/EratosthenesPrimeSieve.java
+++ EratosthenesPrimeSieve/src/net/htlgkr/pos3/FriedwagnerS22040/EratosthenesPrimeSieve.java
@@ -2,5 +2,21 @@ package net.htlgkr.pos3.FriedwagnerS22040;
 
 public class EratosthenesPrimeSieve
 {
+    private int limit;
 
+    public EratosthenesPrimeSieve(int limit) {
+        this.limit = limit;
+    }
+
+    public boolean isPrime(int p)
+    {
+
+
+        return true;
+    }
+
+    public void printPrime()
+    {
+
+    }
 }
